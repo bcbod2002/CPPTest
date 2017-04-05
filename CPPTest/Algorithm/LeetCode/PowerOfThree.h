423e3fd95fd60c58d6ab5cd03d0504cd0c264266
Goston <bcbod2002@gmail.com>
2017-04-05 21:35:58 +0800

    Add : PowerOfThree in LeetCode.

---
+++ CPPTest/Algorithm/LeetCode/PowerOfThree.h
@@ -0,0 +1,39 @@
+//
+//  PowerOfThree.h
+//  CPPTest
+//
+//  Created by Goston on 05/04/2017.
+//  Copyright © 2017 Goston. All rights reserved.
+//
+
+#ifndef PowerOfThree_h
+#define PowerOfThree_h
+
+struct PowerOfThree {
+public:
+    bool isPowerOfThree(int n) {
+        if (n < 0) {
+            return false;
+        }
+        else if (n == 1) {
+            return true;
+        }
+        
+        int last = n;
+        
+        while(last > 0 ) {
+            if (last % 3 == 0) {
+                last = last / 3;
+            }
+            else {
+                return false;
+            }
+            
+            if (last == 1) {
+                return true;
+            }
+        }
+    }
+};
+
+#endif /* PowerOfThree_h */
